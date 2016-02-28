3e610d0ddf2bfa0ace59eb1df598be905b409557
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-02-28 14:51:34 -0300

    retoques al dialogo

--- social-pricing/app/src/main/java/org/pasut/android/socialpricing/InitActivity.java
+++ social-pricing/app/src/main/java/org/pasut/android/socialpricing/InitActivity.java
@@ -1,17 +1,47 @@
 package org.pasut.android.socialpricing;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Parcelable;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class InitActivity extends AppCompatActivity {
+    public final static String LOCATION_EVENT = "location_event";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
+        View locationButton = findViewById(R.id.locationButton);
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO Por ahora mockeo la llamada al servicio
+                Intent intent = new Intent(LOCATION_EVENT);
+                intent.putExtra("data", new Parcelable[]{});
+                LocalBroadcastManager.getInstance(InitActivity.this).sendBroadcast(intent);
+            }
+        });
+        LocalBroadcastManager.getInstance(this).registerReceiver(locationReceiver,
+                new IntentFilter(LOCATION_EVENT));
     }
+
+    @Override
+    protected void onDestroy() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(locationReceiver);
+        super.onDestroy();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,4 +64,26 @@ public class InitActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private final BroadcastReceiver locationReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            new AlertDialog.Builder(InitActivity.this)
+                    .setMessage(R.string.no_market_location)
+                    .setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            Toast.makeText(InitActivity.this, "Sorry, not implemented yet :(", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .setNegativeButton(R.string.discard, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create().show();
+        }
+    };
 }
