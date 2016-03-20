a7672ab87ef8078490a56b4157c3ea66f0c10096
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-03-20 20:29:04 -0300

    Market service ya tiene el RestService, tengo que implementar la llamda

--- social-pricing/app/src/main/java/org/pasut/android/socialpricing/services/MarketService.java
+++ social-pricing/app/src/main/java/org/pasut/android/socialpricing/services/MarketService.java
@@ -8,6 +8,8 @@ import android.support.v4.content.LocalBroadcastManager;
 
 import com.google.common.base.Preconditions;
 
+import org.pasut.android.socialpricing.SocialPriceApplication;
+
 /**
  * Created by boot on 3/20/16.
  */
@@ -17,14 +19,16 @@ public class MarketService {
     public final static String SEARCH_SEARCH_EVENT = "search_event";
 
     private Context context;
+    private RestService restService;
 
     public MarketService(final Context context) {
-        
+        SocialPriceApplication app = (SocialPriceApplication) context.getApplicationContext();
+        this.restService = app.getRestService();
         this.context = Preconditions.checkNotNull(context);
     }
 
     public void searchByAddress(final String address, final String locale) {
-        //TODO Implement request
+        //TODO Implement request ya tengo el rest service, solo tengo que implementar la llamada
         Intent intent = new Intent(SEARCH_SEARCH_EVENT);
         intent.putExtra("data", new Parcelable[]{});
         LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
@@ -32,5 +36,6 @@ public class MarketService {
 
     public void destroy() {
         this.context = null;
+        this.restService = null;
     }
 }
