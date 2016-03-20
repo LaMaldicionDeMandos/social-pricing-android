4bd0b5d7eb26fe594fb7f878358cf2a32cd91d6e
Marcelo Pasut <pasutmarcelo@gmail.com>
2016-03-20 11:46:11 -0300

    Empiezo a trabajar en el Market service

--- social-pricing/app/src/main/java/org/pasut/socialpricing/activities/InitActivity.java
+++ social-pricing/app/src/main/java/org/pasut/socialpricing/activities/InitActivity.java
@@ -1,4 +1,4 @@
-package org.pasut.android.socialpricing;
+package org.pasut.socialpricing.activities;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -15,14 +15,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class InitActivity extends AppCompatActivity {
-    public final static String LOCATION_EVENT = "location_event";
-    public final static String FAVORITE_EVENT = "favorite_event";
-    public final static String SEARCH_EVENT = "search_event";
+import org.pasut.android.socialpricing.R;
+import org.pasut.socialpricing.services.MarketService;
 
+import static org.pasut.socialpricing.services.MarketService.FAVORITE_SEARCH_EVENT;
+import static org.pasut.socialpricing.services.MarketService.LOCATION_SEARCH_EVENT;
+import static org.pasut.socialpricing.services.MarketService.SEARCH_SEARCH_EVENT;
+
+public class InitActivity extends AppCompatActivity {
+    private MarketService marketService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        marketService = new MarketService(this);
         setContentView(R.layout.activity_init);
         View locationButton = findViewById(R.id.locationButton);
         View favoriteButton = findViewById(R.id.favoriteButton);
@@ -31,7 +36,7 @@ public class InitActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //TODO Por ahora mockeo la llamada al servicio
-                Intent intent = new Intent(LOCATION_EVENT);
+                Intent intent = new Intent(LOCATION_SEARCH_EVENT);
                 intent.putExtra("data", new Parcelable[]{});
                 LocalBroadcastManager.getInstance(InitActivity.this).sendBroadcast(intent);
             }
@@ -40,7 +45,7 @@ public class InitActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //TODO Por ahora mockeo la llamada al servicio
-                Intent intent = new Intent(FAVORITE_EVENT);
+                Intent intent = new Intent(FAVORITE_SEARCH_EVENT);
                 intent.putExtra("data", new Parcelable[]{});
                 LocalBroadcastManager.getInstance(InitActivity.this).sendBroadcast(intent);
             }
@@ -48,18 +53,15 @@ public class InitActivity extends AppCompatActivity {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO Por ahora mockeo la llamada al servicio
-                Intent intent = new Intent(SEARCH_EVENT);
-                intent.putExtra("data", new Parcelable[]{});
-                LocalBroadcastManager.getInstance(InitActivity.this).sendBroadcast(intent);
+                showSearchByAddressDiaglo();
             }
         });
         LocalBroadcastManager.getInstance(this).registerReceiver(locationReceiver,
-                new IntentFilter(LOCATION_EVENT));
+                new IntentFilter(LOCATION_SEARCH_EVENT));
         LocalBroadcastManager.getInstance(this).registerReceiver(favoriteReceiver,
-                new IntentFilter(FAVORITE_EVENT));
+                new IntentFilter(FAVORITE_SEARCH_EVENT));
         LocalBroadcastManager.getInstance(this).registerReceiver(searchReceiver,
-                new IntentFilter(SEARCH_EVENT));
+                new IntentFilter(SEARCH_SEARCH_EVENT));
     }
 
     @Override
@@ -67,6 +69,9 @@ public class InitActivity extends AppCompatActivity {
         LocalBroadcastManager.getInstance(this).unregisterReceiver(locationReceiver);
         LocalBroadcastManager.getInstance(this).unregisterReceiver(favoriteReceiver);
         LocalBroadcastManager.getInstance(this).unregisterReceiver(searchReceiver);
+
+        marketService.destroy();
+
         super.onDestroy();
     }
 
@@ -120,7 +125,8 @@ public class InitActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
-                        Toast.makeText(InitActivity.this, "Sorry, not implemented yet :(", Toast.LENGTH_LONG).show();
+                        //TODO Como obtengo los valores de los cuadros de texto?
+                        marketService.searchByAddress("Bla", "Ble");
                     }
                 })
                 .setNegativeButton(R.string.discard, new DialogInterface.OnClickListener() {
@@ -143,7 +149,8 @@ public class InitActivity extends AppCompatActivity {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            showSearchByAddressDiaglo();
+            //TODO ver que me viene jeje
+            showEmptyDialog(R.string.no_market_location);
         }
     };
 
---
+++ social-pricing/app/src/main/java/org/pasut/socialpricing/services/MarketService.java
@@ -0,0 +1,34 @@
+package org.pasut.socialpricing.services;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.support.v4.content.LocalBroadcastManager;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * Created by boot on 3/20/16.
+ */
+public class MarketService {
+    public final static String LOCATION_SEARCH_EVENT = "location_event";
+    public final static String FAVORITE_SEARCH_EVENT = "favorite_event";
+    public final static String SEARCH_SEARCH_EVENT = "search_event";
+
+    private Context context;
+
+    public MarketService(final Context context) {
+        this.context = Preconditions.checkNotNull(context);
+    }
+
+    public void searchByAddress(final String address, final String locale) {
+        //TODO Implement request
+        Intent intent = new Intent(SEARCH_SEARCH_EVENT);
+        intent.putExtra("data", new Parcelable[]{});
+        LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+    }
+
+    public void destroy() {
+        this.context = null;
+    }
+}
